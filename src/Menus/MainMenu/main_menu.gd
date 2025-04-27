9ebbf657fa038ae5228f581ec52071bcbf3f7831
SP4R0W <sp4r0wxxx@gmail.com>
2025-04-27 22:06:52 +0200

    Changed version text

--- src/Menus/MainMenu/main_menu.gd
+++ src/Menus/MainMenu/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var version_text: Label = $VersionText
+@onready var version_text: Label = $MarginContainer/VersionText
 @onready var highscore_text: Label = $HighscoreText
 
 func _ready() -> void:
@@ -12,18 +12,7 @@ func _ready() -> void:
 	if OS.get_name() == "Web":
 		$ButtonContainer/QuitButton.hide()
 
-	set_version_text()
-
-func set_version_text() -> void:
-	var version: String = ProjectSettings.get_setting("application/config/version") as String
-	var how_many_zeroes: int = version.count("0")
-
-	if how_many_zeroes == 1:
-		version_text.text = "v" + version.trim_suffix(".0")
-	elif how_many_zeroes > 1:
-		version_text.text = "v" + version.trim_suffix(".0.0")
-	else:
-		version_text.text = "v" + version
+	version_text.text = "v" + ProjectSettings.get_setting("application/config/version")
 
 func _on_play_button_pressed() -> void:
 	Globals.menu_theme.stop()
